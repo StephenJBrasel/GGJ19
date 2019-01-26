72a5b5c430d3cc140d5a84945c6b9d41696244d6
Mike <mdsiegfried@fullsail.edu>
2019-01-26 12:14:12 -0500

    I'm not sure what this commit is.

--- Follow Me Home/Assets/Scripts/enemyMovement.cs
+++ Follow Me Home/Assets/Scripts/enemyMovement.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 public class enemyMovement : MonoBehaviour
 {
 
-    public Transform owner;
     public Transform[] waypoints;
     public float speed;
 
     private bool toNextWaypoint = true;
-    private CharacterController controller;
 
     private int currentWaypoint = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+
         if (currentWaypoint < waypoints.Length)
         {
             moveToNextWaypoint();
@@ -31,12 +31,10 @@ public class enemyMovement : MonoBehaviour
 
     private void moveToNextWaypoint()
     {
-        Vector3 nextWaypoint = waypoints[currentWaypoint].position;
-        Vector3 direction = nextWaypoint - transform.position;
-    }
-
-    private void FixedUpdate()
-    {
-        owner.Translate(Vector3.right * Time.deltaTime * speed);
+        if (toNextWaypoint)
+        {
+            Vector3 nextWaypoint = waypoints[currentWaypoint].position;
+            Vector3 direction = nextWaypoint - transform.position;
+        }
     }
 }
