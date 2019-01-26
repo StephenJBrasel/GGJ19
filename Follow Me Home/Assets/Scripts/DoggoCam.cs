4d8f2b5e5f08600d8748a9bb4f86999f2b02d455
Jehanlos <me@jehanlos.com>
2019-01-26 17:20:31 -0500

    Camera now only moves in X

--- Follow Me Home/Assets/Scripts/DoggoCam.cs
+++ Follow Me Home/Assets/Scripts/DoggoCam.cs
@@ -8,22 +8,20 @@ public class DoggoCam : MonoBehaviour
     public float smoothTime = 0.1f;
     public float maxSpeed = 10.0f;
 
-    private Vector3 offset = Vector3.zero;
-
-    private Vector3 currentVelocity = Vector3.zero;
+    private float xOffset = 0.0f;
+    private float currentXVelocity = 0.0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = gameObject.transform.position - doggo.transform.position;
+        xOffset = transform.position.x - doggo.transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPosition = doggo.transform.position + offset;
-        targetPosition = Vector3.SmoothDamp(gameObject.transform.position, targetPosition, ref currentVelocity, smoothTime, maxSpeed);
-        gameObject.transform.position = targetPosition;
+        float newX = Mathf.SmoothDamp(transform.position.x, doggo.transform.position.x + xOffset, ref currentXVelocity, smoothTime, maxSpeed);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
 }
