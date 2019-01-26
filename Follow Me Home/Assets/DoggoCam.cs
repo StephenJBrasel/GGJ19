bcb9ff22ec9a2ef304a21f56a5bf87675cf4c93d
Jehanlos <me@jehanlos.com>
2019-01-25 22:44:47 -0500

    simple camera and movement

---
+++ Follow Me Home/Assets/DoggoCam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoggoCam : MonoBehaviour
+{
+    public GameObject doggo;
+    public float smoothTime = 0.1f;
+    public float maxSpeed = 10.0f;
+
+    private Vector3 offset = Vector3.zero;
+
+    private Vector3 currentVelocity = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = gameObject.transform.position - doggo.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = doggo.transform.position + offset;
+        targetPosition = Vector3.SmoothDamp(gameObject.transform.position, targetPosition, ref currentVelocity, smoothTime, maxSpeed);
+        gameObject.transform.position = targetPosition;
+    }
+}
---
+++ Follow Me Home/Assets/DoggoController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoggoController : MonoBehaviour
+{
+    public float walkSpeed = 5.0f;
+    public float sprintSpeed = 10.0f;
+    public KeyCode furtherKey = KeyCode.W;
+    public KeyCode closerKey = KeyCode.S;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public float smoothTime = 0.1f;
+    public float maxSpeed = 10.0f;
+
+
+    public float maxZ = 4.0f;
+    public float minZ = 0.0f;
+
+    private float currentZVelocity = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetKey(sprintKey) ? sprintSpeed : walkSpeed;
+
+        Move(Time.deltaTime * speed, 0.0f, 0.0f);
+
+        if (Input.GetKeyDown(furtherKey))
+        {
+            Move(0.0f, 0.0f, 1.0f);
+        }
+
+        if (Input.GetKeyDown(closerKey))
+        {
+            Move(0.0f, 0.0f, -1.0f);
+        }
+    }
+
+    void Move(Vector3 delta)
+    {
+        Move(delta.x, delta.y, delta.z);
+    }
+
+    void Move(float deltaX, float deltaY, float deltaZ)
+    {
+        Vector3 newPosition = new Vector3(gameObject.transform.position.x + deltaX, gameObject.transform.position.y + deltaY, gameObject.transform.position.z + deltaZ);
+        newPosition = new Vector3(newPosition.x, newPosition.y, Mathf.Clamp(newPosition.z, minZ, maxZ));
+        //newPosition.z = Mathf.SmoothDamp(gameObject.transform.position.z, newPosition.z, ref currentZVelocity, smoothTime, maxSpeed);
+        gameObject.transform.position = newPosition;
+    }
+}
