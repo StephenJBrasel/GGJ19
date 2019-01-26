6332b54bb73b44aee1fd43589857b308c1275557
Siddharth Jain <me@theinsomniacgamemaker.com>
2019-01-25 22:44:03 -0500

    Started Owner AI

---
+++ Follow Me Home/Assets/OwnerDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnerDetector : MonoBehaviour
+{
+    OwnerAI selfAIScript;
+    private void Start()
+    {
+        selfAIScript = transform.parent.GetComponent<OwnerAI>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Dog")
+        {
+            selfAIScript.suspicionLevel+=0.1f;   
+        }
+    }
+}
---
+++ Follow Me Home/Assets/Scripts/OwnerAI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnerAI : MonoBehaviour
+{
+    bool isTurnningAround = false;
+    [SerializeField]
+    float[] triggerPoints;
+
+    [SerializeField]
+    GameObject dog;
+    Detector dogDetectorScript;
+    public float suspicionLevel;
+
+    private void Start()
+    {
+        dogDetectorScript = GetComponent<Detector>();   
+    }
+
+
+    private void Update()
+    {
+
+        Debug.Log("Suspicion Level: " + suspicionLevel);
+        if (suspicionLevel >= 10.0f)
+        {
+            isTurnningAround = true;
+               suspicionLevel = 0.0f;
+        }
+
+        if (!isTurnningAround)
+        {
+            transform.Translate(2.0f * Time.deltaTime, 0, 0);
+        }
+
+        if (isTurnningAround)
+        {
+            if (dogDetectorScript.isHiding)
+            {
+                Debug.Log("Serached for dog but didn't see anything");
+            }
+            else
+            {
+                Debug.Log("Game Over");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("SuspicionTrigger") && suspicionLevel<10.0f)
+        {
+            suspicionLevel = 10.0f;
+        }
+    }
+
+
+
+
+
+
+}
