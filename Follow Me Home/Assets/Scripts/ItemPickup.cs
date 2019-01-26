622c7542f3ce1edbf29cb7b2f2d350e8dca8e25a
StephenJBrasel <stephen.brasel@gmail.com>
2019-01-26 14:53:12 -0500

    Made Pause Menu Prefab, Item Pickup script.

---
+++ Follow Me Home/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+	[SerializeField]
+	public float power;
+	public GameObject item;
+
+	private void Start()
+	{
+		item = GetComponent<GameObject>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			DoggoController controller = other.GetComponent<DoggoController>();
+			controller.walkSpeed += power;
+			item.SetActive(false);
+		}
+	}
+}
