86260c8da3a4ce07f49a73dba2115276ce5ddbd1
StephenJBrasel <stephen.brasel@gmail.com>
2019-01-26 11:24:52 -0500

    Ending the level unlocks the next level, loads it.

---
+++ Follow Me Home/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+	public string sceneName;
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Player")
+		{
+			PlayerPrefs.SetInt(sceneName, 1);
+			UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+		}
+	}
+
+}
