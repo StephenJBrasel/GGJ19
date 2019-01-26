5c67ba3942be2fc2076678a28dd149887595c9a7
StephenJBrasel <stephen.brasel@gmail.com>
2019-01-26 18:42:40 -0500

    StaminaBar works, Menus prefab covers GUI and gameover/pause menus.
    Stamina bar accurately reflects the stamina of the player,
    Menus can be added to any level as a prefab now.

---
+++ Follow Me Home/Assets/Scripts/MenuStaminaSlider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuStaminaSlider : MonoBehaviour
+{
+	public GameObject doggo;
+	public Slider staminaBar;
+
+	private DoggoController controller;
+
+	private void Start()
+	{
+		controller = doggo.GetComponent<DoggoController>();
+		staminaBar.maxValue = controller.staminaThreshold;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		staminaBar.value = controller.stamina;
+	}
+}
