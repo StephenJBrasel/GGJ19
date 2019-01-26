4805db15106b8558d25f9db70a108160ce1af3d6
Siddharth Jain <me@theinsomniacgamemaker.com>
2019-01-25 20:26:04 -0500

    Simple Movement for Testing Complete
    The cube is mvong and the camera with it.

---
+++ Follow Me Home/Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(Mathf.Abs(Input.GetAxis("Horizontal")*10), 0, Mathf.Abs(Input.GetAxis("Vertical")*10));
+    }
+}
