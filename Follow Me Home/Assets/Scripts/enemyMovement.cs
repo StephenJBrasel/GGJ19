7f9fc2515bd0b90e1eeb05064f3cf63f352d5396
Mike <mdsiegfried@fullsail.edu>
2019-01-25 23:55:10 -0500

    Update .gitignore. Add character controller.

--- Follow Me Home/Assets/Scripts/enemyMovement.cs
+++ Follow Me Home/Assets/Scripts/enemyMovement.cs
@@ -8,14 +8,16 @@ public class enemyMovement : MonoBehaviour
     public Transform owner;
     public Transform[] waypoints;
     public float speed;
+
     private bool toNextWaypoint = true;
+    private CharacterController controller;
 
     private int currentWaypoint = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
