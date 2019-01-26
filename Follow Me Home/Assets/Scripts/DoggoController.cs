1e119b82fef7d37b00986faf28b623a98b60c2a1
StephenJBrasel <stephen.brasel@gmail.com>
2019-01-26 08:50:03 -0500

    Doggo can't stay still for long, runs out of energy when running.
    Stamina builds when the player stays still, stamina runs out as the player sprints. Players have to make tactical stealth decisions.

--- Follow Me Home/Assets/Scripts/DoggoController.cs
+++ Follow Me Home/Assets/Scripts/DoggoController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DoggoController : MonoBehaviour
 {
+	public float staminaThreshold = 20.0f;
+	public float staminaDiff = 0.5f;
+	public float stamina = 10.0f;
+
     public float walkSpeed = 5.0f;
     public float sprintMultiplier = 2.0f;
     public KeyCode furtherKey = KeyCode.W;
@@ -27,10 +31,24 @@ public class DoggoController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float speed = Input.GetKey(stopKey) ? 0.0f : walkSpeed;
-        if (Input.GetKey(sprintKey))
+		float speed = walkSpeed;
+		if (Input.GetKey(stopKey) && stamina < staminaThreshold)
+		{
+			speed = 0.0f;
+			stamina += staminaDiff;
+			if (stamina > staminaThreshold)
+			{
+				stamina = staminaThreshold;
+			}
+		}
+        if (Input.GetKey(sprintKey) && stamina > 0.0f)
         {
             speed *= sprintMultiplier;
+			stamina -= staminaDiff;
+			if (stamina < 0.0f)
+			{
+				stamina = 0.0f;
+			}
         }
 
         Move(Time.fixedDeltaTime * speed, 0.0f, 0.0f);
