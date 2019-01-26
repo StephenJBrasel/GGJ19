be246764b53c16a78157dfc632b059e961cd3283
Mike <mdsiegfried@fullsail.edu>
2019-01-25 22:22:39 -0500

    Enemy moving forward
    Enemy is moving in forward in the x-axis.

---
+++ Follow Me Home/Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+
+    public Transform owner;
+    public Vector2 velocity;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        owner.Translate(Vector3.right * Time.deltaTime);
+    }
+}
