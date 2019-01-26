e5455217f16547f9b50a75445455333db9e67673
StephenJBrasel <stephen.brasel@gmail.com>
2019-01-26 18:13:49 -0500

    Refactoring names, pit game overs are not going to be implemented.

--- Follow Me Home/Assets/Scripts/Menu_GameOver.cs
+++ Follow Me Home/Assets/Scripts/Menu_GameOver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Pit_GameOver : MonoBehaviour
+public class Menu_GameOver : MonoBehaviour
 {
 	private void Start()
 	{
