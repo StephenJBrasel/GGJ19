0c888c64786ad388985f9165758bfee2f89c3d23
StephenJBrasel <stephen.brasel@gmail.com>
2019-01-26 21:38:08 -0500

    Lucas Level 3, navmeshes working.

--- Follow Me Home/Assets/Scripts/enemyMovement.cs
+++ Follow Me Home/Assets/Scripts/enemyMovement.cs
@@ -13,7 +13,7 @@ public class enemyMovement : MonoBehaviour
     private int destination = 0;
     private bool isSearching = false;
     private bool isAlerted = false;
-    private float remainingDistance = 0.0f;
+    private float remainingDistance = float.MaxValue;
 
     // Start is called before the first frame update
     void Start()
