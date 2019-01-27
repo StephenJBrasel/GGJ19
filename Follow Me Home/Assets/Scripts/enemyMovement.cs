c14d125e9477dc06b6cc08d0da8c63b00beb963d
Mike <mdsiegfried@fullsail.edu>
2019-01-26 19:41:00 -0500

    Commit to switch branch

--- Follow Me Home/Assets/Scripts/enemyMovement.cs
+++ Follow Me Home/Assets/Scripts/enemyMovement.cs
@@ -7,9 +7,11 @@ public class enemyMovement : MonoBehaviour
 {
 
     public Transform[] waypoints;
-    
+    public Detector detector;
     private NavMeshAgent agent;
     private int destination = 0;
+    private bool isSearching = false;
+    private bool isAlerted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +24,7 @@ public class enemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!agent.pathPending && agent.remainingDistance < 0.5f)
+        if(!agent.pathPending && agent.remainingDistance < 0.5f && !isAlerted)
         {
             moveToNextWaypoint();
         }
@@ -34,11 +36,42 @@ public class enemyMovement : MonoBehaviour
         // Set the way point for the NPC to move to.
         agent.SetDestination(waypoints[destination].position);
 
-        // Set the next waypoint in the array.
-        destination = ++destination % waypoints.Length;
-        Debug.Log("Alerted");
-        Debug.Log("Turn Around");
+        //// Set the next waypoint in the array.
+        //destination = ++destination % waypoints.Length;
+        if (destination == (waypoints.Length - 1))
+        {
+            Debug.Log("Waypoints Len: ");
+            Debug.Log(waypoints.Length - 1);
+            agent.isStopped = true;
+        }
 
+        if (agent.remainingDistance < 0.2f)
+        {
+            Debug.Log(agent.remainingDistance);
+            //alerted();
+        }
+    }
+
+    private void alerted()
+    {
+        isAlerted = true;
+        Debug.Log("I am alerted!");
+        agent.isStopped = true;
+        turnAround();
+    }
+
+    private void turnAround()
+    {
+        if (!detector.isHiding)
+        {
+            Debug.Log("I SEE YOU!");
+        }
+        else
+        {
+            isAlerted = false;
+            agent.isStopped = false;
+            Debug.Log("Must have been nothing.");
+        }
     }
 
 }
