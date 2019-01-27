c53242bb7601c4380068e7f8700c6e9265a5bb3e
Jehanlos <me@jehanlos.com>
2019-01-26 19:01:37 -0500

    Added Obstacle prefab

---
+++ Follow Me Home/Assets/Obstacle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Obstacle : MonoBehaviour
+{
+    public enum Type
+    {
+        BikeRack,
+        FireHydrant,
+        Scaffold,
+        ShopSign,
+        TrashCan
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[CustomEditor(typeof(Obstacle))]
+public class ObstacleEditor : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        Obstacle myTarget = (Obstacle)target;
+        
+        string current = myTarget.transform.GetChild(0).name;
+        for (int i = 0; i < myTarget.transform.childCount; ++i)
+        {
+            var child = myTarget.transform.GetChild(i);
+            if (child.gameObject.activeSelf)
+            {
+                current = child.name;
+                break;
+            }
+        }
+
+        var type = EditorGUILayout.EnumPopup("Type", (Obstacle.Type)System.Enum.Parse(typeof(Obstacle.Type), current), GUIStyle.none, null);
+        for (int j = 0; j < myTarget.transform.childCount; ++j)
+        {
+            var child = myTarget.transform.GetChild(j);
+            child.gameObject.SetActive(child.name == type.ToString());
+        }
+    }
+}
