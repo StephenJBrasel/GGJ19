345c94941ad0e0c72af433a5a3925e4b7f92c293
StephenJBrasel <stephen.brasel@gmail.com>
2019-01-27 11:11:04 -0500

    UI Sounds - Game over and Pause menus.

--- Follow Me Home/Assets/Scripts/MenuPause.cs
+++ Follow Me Home/Assets/Scripts/MenuPause.cs
@@ -7,9 +7,16 @@ public class MenuPause : MonoBehaviour
 {
 	public bool GameIsPaused = false;
 	public GameObject PauseMenuUI;
+	private AudioSource AudioSource;
+	public AudioClip click;
 
-    // Update is called once per frame
-    void Update()
+	private void Start()
+	{
+		AudioSource = gameObject.GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
 		{
@@ -29,6 +36,7 @@ public class MenuPause : MonoBehaviour
 		PauseMenuUI.SetActive(false);
 		Time.timeScale = 1f;
 		GameIsPaused = false;
+		AudioSource.PlayOneShot(click);
 	}
 
 	public void Pause()
@@ -36,25 +44,29 @@ public class MenuPause : MonoBehaviour
 		PauseMenuUI.SetActive(true);
 		Time.timeScale = 0f;
 		GameIsPaused = true;
+		AudioSource.PlayOneShot(click);
 	}
 
 	public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         Time.timeScale = 1f;
-    }
+		AudioSource.PlayOneShot(click);
+	}
 
 	public void LoadMenu()
 	{
 		Debug.Log("Loading...");
 		Time.timeScale = 1f;
 		SceneManager.LoadScene("MainMenu");
+		AudioSource.PlayOneShot(click);
 	}
 
 	public void QuitGame()
 	{
 		Debug.Log("Quitting...");
 		Application.Quit();
+		AudioSource.PlayOneShot(click);
 	}
 
 }
