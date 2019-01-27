dd26564974f244729146f9ad7a7585c90cc2b4b2
Jehanlos <me@jehanlos.com>
2019-01-27 12:09:03 -0500

    Enemy rotating (state machine)

--- Follow Me Home/Assets/Scripts/enemyMovement.cs
+++ Follow Me Home/Assets/Scripts/enemyMovement.cs
@@ -7,12 +7,15 @@ using UnityEngine.Animations;
 public class enemyMovement : MonoBehaviour
 {
 
-    private enum AlertLevel
+    private enum State
     {
         Chill,
         Suspicious,
-        Alerted
+        Stopping,
+        Stopped,
+        Resuming
     }
+
 
     public Transform waypointsParent;
     public Detector detector;
@@ -21,11 +24,14 @@ public class enemyMovement : MonoBehaviour
     public Menu_GameOver gameOver;
     public float smoothing = 0.1f;
     public GameObject doggo;
+    public float degreesPerSecond = 720.0f;
+    public float stopDuration = 1.0f;
 
     private NavMeshAgent agent;
     private int destination;
-    private bool isAlerted = false;
     private float remainingDistance = float.MaxValue;
+    private State state = State.Chill;
+    private float stopTime;
 
     // Start is called before the first frame update
     void Start()
@@ -39,98 +45,118 @@ public class enemyMovement : MonoBehaviour
     void Update()
     {
         remainingDistance = waypointsParent.GetChild(destination).position.x - agent.transform.position.x;
-        if (!agent.pathPending && !isAlerted)
+
+        switch (state)
         {
-            if (remainingDistance < 0.25f)
+            case State.Chill:
+                if (remainingDistance < 10.0f)
+                {
+                    SetState(State.Suspicious);
+                }
+                break;
+            case State.Suspicious:
+                if (remainingDistance < 0.1f)
+                {
+                    // Set the way point for the NPC to move to.
+                    if (destination < (waypointsParent.childCount - 1))
+                    {
+                        destination++;
+                        agent.SetDestination(waypointsParent.GetChild(destination).position);
+                    }
+                    // If enemy is at the last way point stop
+                    else if (destination == (waypointsParent.childCount - 1))
+                    {
+                        Debug.Log("Destination: " + destination.ToString());
+                        Debug.Log("Waypoints Len: " + waypointsParent.childCount.ToString());
+                    }
+
+                    SetState(State.Stopping);
+                }
+                break;
+            case State.Stopping:
             {
-                moveToNextWaypoint();
+                Quaternion toDoggo = Quaternion.LookRotation((doggo.transform.position - transform.position).normalized, Vector3.up);
+                Quaternion newRotation = Quaternion.RotateTowards(transform.rotation, toDoggo, Time.deltaTime * degreesPerSecond);
+                if (Quaternion.Angle(transform.rotation, newRotation) > 5.0f)
+                {
+                    transform.rotation = newRotation;
+                }
+                else
+                {
+                    SetState(State.Stopped);
+                }
+                break;
             }
-            else if (remainingDistance < 10.0f)
+            case State.Stopped:
+                if (((Time.time - stopTime) > stopDuration) && gameOver.isActiveAndEnabled == false)
+                {
+                    Debug.Log("Stopped");
+                    if (detector.isHiding)
+                    {
+                        SetState(State.Resuming);
+                    }
+                    else
+                    {
+                        Debug.Log("I SEE YOU!");
+                        gameOver.gameObject.SetActive(true);
+                    }
+                }
+                break;
+            case State.Resuming:
             {
-                SetAlertLevel(AlertLevel.Suspicious);
+                Quaternion toDoggo = Quaternion.LookRotation((doggo.transform.position - transform.position).normalized, Vector3.up);
+                Quaternion newRotation = Quaternion.RotateTowards(transform.rotation, toDoggo, Time.deltaTime * degreesPerSecond);
+                if (Quaternion.Angle(transform.rotation, newRotation) > 5.0f)
+                {
+                    transform.rotation = newRotation;
+                }
+                else
+                {
+                    SetState(State.Chill);
+                }
+                break;
             }
         }
-
     }
 
-    private void SetAlertLevel(AlertLevel alertLevel)
+    private void SetState(State newState)
     {
-        switch (alertLevel)
+        switch (newState)
         {
-            case AlertLevel.Chill:
+            case State.Chill:
                 suspiciousIcon.SetActive(false);
                 alertedIcon.SetActive(false);
+                agent.isStopped = false;
+                state = State.Chill;
                 break;
-            case AlertLevel.Suspicious:
+            case State.Suspicious:
+                Debug.Log("Did I hear something?");
                 suspiciousIcon.SetActive(true);
                 alertedIcon.SetActive(false);
+                agent.isStopped = false;
+                state = State.Suspicious;
                 break;
-            case AlertLevel.Alerted:
+            case State.Stopping:
+                suspiciousIcon.SetActive(true);
+                alertedIcon.SetActive(false);
+                agent.isStopped = true;
+                state = State.Stopping;
+                break;
+            case State.Stopped:
+                Debug.Log("Who goes there?");
                 suspiciousIcon.SetActive(false);
                 alertedIcon.SetActive(true);
+                agent.isStopped = true;
+                state = State.Stopped;
+                stopTime = Time.time;
+                break;
+            case State.Resuming:
+                Debug.Log("Must have been nothing.");
+                suspiciousIcon.SetActive(false);
+                alertedIcon.SetActive(false);
+                agent.isStopped = true;
+                state = State.Resuming;
                 break;
         }
     }
-
-    private void moveToNextWaypoint()
-    {
-        // Set the way point for the NPC to move to.
-        if (destination < (waypointsParent.childCount - 1))
-        {
-            destination++;
-            agent.SetDestination(waypointsParent.GetChild(destination).position);
-        }
-
-        // If enemy is at the last way point stop
-        else if (destination == (waypointsParent.childCount - 1))
-        {
-            Debug.Log("Destination: " + destination.ToString());
-            Debug.Log("Waypoints Len: " + waypointsParent.childCount.ToString());
-            agent.isStopped = true;
-        }
-
-        //if (destination % 2 == 0)
-        {
-            Debug.Log("Remaining Distance: " + remainingDistance.ToString());
-            alerted();
-        }
-    }
-
-    private void alerted()
-    {
-        isAlerted = true;
-        SetAlertLevel(AlertLevel.Alerted);
-        Debug.Log("I am alerted!");
-        turnAround();
-    }
-
-    private void turnAround()
-    {
-        agent.isStopped = true;
-        RotateTowards(doggo.transform);
-
-        bool rotationComplete = true;
-        bool rotatedBack = false;
-        if (!detector.isHiding && rotationComplete)
-        {
-            Debug.Log("I SEE YOU!");
-            gameOver.enabled = true;
-        }
-        else if (rotatedBack)
-        {
-            isAlerted = false;
-            SetAlertLevel(AlertLevel.Chill);
-            agent.isStopped = false;
-            Debug.Log("Must have been nothing.");
-        }
-    }
-
-    private void RotateTowards(Transform target)
-    {
-        Debug.Log("Rotating");
-        Vector3 direction = (target.position - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * smoothing);
-    }
-
 }
